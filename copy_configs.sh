626fdfd10757603332c1c24aab4327206b1f153e
Pial Das <pdas2711@proton.me>
2024-06-01 16:23:44 -0400

    Added waybar to coopy script.

--- copy_configs.sh
+++ copy_configs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-config_dirs=("hypr" "nvim" "tmux" "zsh" "foot")
+config_dirs=("hypr" "nvim" "tmux" "zsh" "foot" "waybar")
 
 for config in "${config_dirs[@]}"; do
 	rsync -av ${XDG_CONFIG_HOME}/${config} . --exclude .git
