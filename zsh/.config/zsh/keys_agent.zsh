8d75b497212100c517f5a515da0693b90c7eba12
Pial Das <pdas2711@proton.me>
2024-06-20 04:39:59 -0400

    Added option for either GPG or SSH as agent.

--- zsh/.config/zsh/keys_agent.zsh
+++ zsh/.config/zsh/keys_agent.zsh
@@ -6,3 +6,6 @@ gpgconf --kill gpg-agent
 export SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
 gpgconf --launch gpg-agent
 gpg-connect-agent updatestartuptty /bye > /dev/null
+
+# Or SSH Agent
+#eval $(ssh-agent)
