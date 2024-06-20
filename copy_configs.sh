cc44629dea884b339af651949e051c45f0c03c46
Pial Das <pdas2711@proton.me>
2024-06-19 22:46:57 -0400

    No longer need this script since Stow takes care of this process.

--- copy_configs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-config_dirs=("hypr" "nvim" "tmux" "zsh" "foot" "waybar")
-
-for config in "${config_dirs[@]}"; do
-	rsync -av ${XDG_CONFIG_HOME}/${config} . --exclude .git
-done
