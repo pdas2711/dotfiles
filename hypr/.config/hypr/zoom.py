3946a3cb99f9a5a87ed8642cfa6e450cd2b78b19
Pial Das <pdas2711@proton.me>
2024-11-06 18:57:26 -0500

    Update to accomodate change in variable group for zoom.

--- hypr/.config/hypr/zoom.py
+++ hypr/.config/hypr/zoom.py
@@ -4,7 +4,7 @@ from sys import argv
 import subprocess
 import json
 
-curr_zoom = json.loads(subprocess.run(["hyprctl -j getoption misc:cursor_zoom_factor"], capture_output = True, text = True, shell = True).stdout)["float"]
+curr_zoom = json.loads(subprocess.run(["hyprctl -j getoption cursor:zoom_factor"], capture_output = True, text = True, shell = True).stdout)["float"]
 if len(argv) != 2:
     exit()
 if argv[1] == "increment":
@@ -17,5 +17,5 @@ elif argv[1] == "standard":
     new_zoom = 1.0
 else:
     exit()
-subprocess.run(["hyprctl keyword misc:cursor_zoom_factor " + str(new_zoom)], shell = True)
+subprocess.run(["hyprctl keyword cursor:zoom_factor " + str(new_zoom)], shell = True)
 exit()
