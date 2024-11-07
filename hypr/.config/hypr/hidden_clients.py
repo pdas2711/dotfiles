355c9b38ada2fcf12f0a80e9fef26fd3c6ca6e99
Pial Das <pdas2711@proton.me>
2024-11-06 20:38:02 -0500

    Made use of grabbing current workspace from activeworkspace rather than parsing output of activewindow.

--- hypr/.config/hypr/hidden_clients.py
+++ hypr/.config/hypr/hidden_clients.py
@@ -13,12 +13,13 @@ if len(argv) == 1:
 if argv[1] == "minimize":
     # Get current workspace name
     window_json = json.loads(subprocess.run(["hyprctl -j activewindow"], capture_output = True, text = True, shell = True).stdout)
-    curr_workspace = window_json["workspace"]["name"]
+    client_address = window_json["address"]
+    workspace_json = json.loads(subprocess.run(["hyprctl -j activeworkspace"], capture_output = True, text = True, shell = True).stdout)
+    curr_workspace = workspace_json["name"]
     print(curr_workspace)
-    if "special" in curr_workspace:
-        parent_workspace = curr_workspace.split(":")[1]
+    if "special" in window_json["workspace"]["name"]:
         client_address = window_json["address"]
-        cmd = "hyprctl dispatch movetoworkspacesilent " + parent_workspace + ",address:" + client_address
+        cmd = "hyprctl dispatch movetoworkspacesilent " + curr_workspace + ",address:" + client_address
         subprocess.run([cmd], shell = True)
     else:
         client_address = window_json["address"]
