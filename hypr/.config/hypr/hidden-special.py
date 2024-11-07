09b2119edd30d40c90daebbd652c0bb78f40b0d8
Pial Das <pdas2711@proton.me>
2024-11-06 20:55:55 -0500

    Added collecting all clients from scratchpad and moving them to current workspace.

--- hypr/.config/hypr/hidden-special.py
+++ hypr/.config/hypr/hidden-special.py
@@ -3,6 +3,7 @@
 from sys import argv
 import json
 import subprocess
+import re
 
 if len(argv) == 1:
     print("Available arguments are: minimize, show, collect")
@@ -19,4 +20,15 @@ if argv[1] == "minimize":
     else:
         cmd = "hyprctl dispatch movetoworkspacesilent special,address:" + client_address
         subprocess.run([cmd], shell = True)
-
+elif argv[1] == "collect":
+    minimized_clients = json.loads(subprocess.run(["hyprctl -j clients"], capture_output = True, text = True, shell = True).stdout)
+    workspace_json = json.loads(subprocess.run(["hyprctl -j activeworkspace"], capture_output = True, text = True, shell = True).stdout)
+    curr_workspace = workspace_json["name"]
+    for client in minimized_clients:
+        client_name = client["workspace"]["name"]
+        if re.search("special:special", client_name):
+            client_address = client["address"]
+            cmd = "hyprctl dispatch movetoworkspace " + curr_workspace + ",address:" + client_address
+            subprocess.run([cmd], shell = True)
+else:
+    exit()
