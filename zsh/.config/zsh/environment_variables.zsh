a3fe4b7300b6e141ae73bac1d4849ed01a8953cd
Pial Das <pdas2711@proton.me>
2025-03-30 20:30:10 -0400

    Added XDG_DATA_DIRS environment variable.

--- zsh/.config/zsh/environment_variables.zsh
+++ zsh/.config/zsh/environment_variables.zsh
@@ -3,6 +3,7 @@
 export XDG_DATA_HOME=${XDG_DATA_HOME:="$HOME/.local/share"}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:="$HOME/.cache"}
 export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:="$HOME/.config"}
+export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/home/pdas2711/.local/share/flatpak/exports/share"
 
 # Additional application file paths
 
