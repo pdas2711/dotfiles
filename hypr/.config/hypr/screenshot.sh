cc3efa7d26adc6d40ba36728514ed45c5928af19
Pial Das <pdas2711@proton.me>
2025-03-30 20:51:39 -0400

    Updated screenshot path.

--- hypr/.config/hypr/screenshot.sh
+++ hypr/.config/hypr/screenshot.sh
@@ -3,7 +3,7 @@
 filename=$(date +%Y_%m_%d-%H_%M_%S)
 
 if [[ "${1}" == "window" ]]; then
-	hyprshot -o $HOME/pictures/screenshots/ -f "${filename}.png" -m window
+	hyprshot -o $HOME/media/pictures/screenshots/ -f "${filename}.png" -m window
 elif [[ "${1}" == "region" ]]; then
-	hyprshot -o $HOME/pictures/screenshots/ -f "${filename}.png" -m region
+	hyprshot -o $HOME/media/pictures/screenshots/ -f "${filename}.png" -m region
 fi
