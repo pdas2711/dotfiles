98174c560455aef2e97f89520ca7673f73943be3
Pial Das <pdas2711@proton.me>
2025-10-25 21:28:22 -0400

    Updated formatting. Functionally unchanged.

--- zsh/.config/zsh/environment_variables.zsh
+++ zsh/.config/zsh/environment_variables.zsh
@@ -7,18 +7,18 @@ export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:${XDG_DATA_HOME}/flatpak/ex
 
 # Additional application file paths
 
-export CARGO_HOME="$XDG_DATA_HOME"/cargo
-export GNUPGHOME="$XDG_DATA_HOME"/gnupg
-export GOPATH="$XDG_DATA_HOME"/go
-export _JAVA_OPTIONS=-Djava.utils.prefs.userRoot="$XDG_CONFIG_HOME"/java
-export GDBHISTFILE="$XDG_CONFIG_HOME"/gdb/history
-export PARALLEL_HOME="$XDG_DATA_HOME"/parallel
-export WGETRC="$XDG_CONFIG_HOME"/wgetrc
-export PYTHONPYCACHEPREFIX="$XDG_CACHE_HOME"/python
-export PYTHONUSERBASE="$XDG_DATA_HOME"/python
-export TRASH="$HOME"/.local/share/Trash/files
-export TEXMFHOME="${XDG_CONFIG_HOME}"/texmf
-export PATH="${PATH}:${HOME}/.local/bin:${HOME}/.local/share/python"
+export CARGO_HOME="${XDG_DATA_HOME}/cargo"
+export GNUPGHOME="${XDG_DATA_HOME}/gnupg"
+export GOPATH="${XDG_DATA_HOME}/go"
+export _JAVA_OPTIONS=-Djava.utils.prefs.userRoot="${XDG_CONFIG_HOME}/java"
+export GDBHISTFILE="${XDG_CONFIG_HOME}/gdb/history"
+export PARALLEL_HOME}="${XDG_DATA_HOME}/parallel"
+export WGETRC="${XDG_CONFIG_HOME}/wgetrc"
+export PYTHONPYCACHEPREFIX="${XDG_CACHE_HOME}/python"
+export PYTHONUSERBASE="${XDG_DATA_HOME}/python"
+export TRASH="${XDG_DATA_HOME}/Trash/files"
+export TEXMFHOME="${XDG_CONFIG_HOME}/texmf"
+export PATH="${PATH}:${HOME}/.local/bin:${XDG_DATA_HOME}/python"
 export BASE16_TMUX_OPTION_ACTIVE=1
 
 # Other environment variables
