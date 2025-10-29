6142e8d50738a9b72e46592287f4c5351009d454
Pial Das <pdas2711@proton.me>
2025-10-28 20:29:57 -0400

    Added function for taking notes using gopass.

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,3 +1,11 @@
 norun() {
 	"$@" &> /dev/null &
 }
+
+function note() {
+	if [[ -z "${1}" ]]; then
+		gopass edit -c notes/$(date +"%Y_%m_%d-%H_%M_%S").md
+	else
+		gopass edit -c notes/"${1}.md"
+	fi
+}
