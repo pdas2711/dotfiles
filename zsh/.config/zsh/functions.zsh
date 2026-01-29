41890a5a35782b1545295bfbe168a67e107bc637
Pial Das <pdas2711@proton.me>
2026-01-29 17:38:42 -0500

    Added function to version control singlular files using git seamlessly.

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -9,3 +9,15 @@ function note() {
 		gopass edit -c notes/"${1}.md"
 	fi
 }
+
+function gitf() {
+	filename="${1}"
+	filerepo="$(pwd | sed 's/\//%/g')%${filename}"
+	if [[ ! -d "${XDG_DATA_HOME}/gitf/${filerepo}" ]]; then
+		mkdir -p "${XDG_DATA_HOME}/gitf/${filerepo}"
+		git init --bare "${XDG_DATA_HOME}/gitf/${filerepo}"
+		echo -e "*\n!${filename}" > "${XDG_DATA_HOME}/gitf/${filerepo}/info/exclude"
+	fi
+	shift
+	git --git-dir="${XDG_DATA_HOME}/gitf/${filerepo}" --work-tree=. "$@"
+}
