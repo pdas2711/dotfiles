a7be2fb85bf548061ceae455c466245039230137
Pial Das <pdas2711@proton.me>
2026-07-23 02:14:19 -0400

    Moved sourcing Home Manager session variables to the ZSH environment configuration.

--- zsh/.config/zsh/environment_variables.zsh
+++ zsh/.config/zsh/environment_variables.zsh
@@ -1,8 +1,3 @@
-# Source Home Manager session variables
-if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
-	. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
-fi
-
 # Basic XDG file paths
 export XDG_DATA_HOME=${XDG_DATA_HOME:="$HOME/.local/share"}
 export XDG_CACHE_HOME=${XDG_CACHE_HOME:="$HOME/.cache"}
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,1 +1,7 @@
+# Source Home Manager session variables
+if [ -e "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh" ]; then
+	. "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
+fi
+
+
 export ZDOTDIR=$HOME/.config/zsh
