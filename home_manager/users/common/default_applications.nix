f67a8d5e2272dbf39e908221eda070854803f5e4
Pial Das <pdas2711@proton.me>
2026-07-30 03:27:56 -0400

    Moved configuration for self-managing home-manager from default_applications to misc.

--- home_manager/users/common/default_applications.nix
+++ home_manager/users/common/default_applications.nix
@@ -49,7 +49,4 @@
 			};
 		};
 	};
-
-	# Let Home Manager install and manage itself.
-	programs.home-manager.enable = true;
 }
--- home_manager/users/common/misc.nix
+++ home_manager/users/common/misc.nix
@@ -10,4 +10,7 @@
 		];
 		config = [ "/etc/xdg" ];
 	};
+
+	# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 }
