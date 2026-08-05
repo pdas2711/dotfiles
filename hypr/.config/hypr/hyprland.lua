b89bb8e2c698bd5c56a28fad213665d0e379d9b4
Pial Das <pdas2711@proton.me>
2026-08-05 12:55:14 -0400

    Added main hyprland config to call other config modules.

---
+++ hypr/.config/hypr/hyprland.lua
@@ -0,0 +1,8 @@
+-- Config location
+let config_dir = "configs_lua"
+
+require(config_dir + ".env_vars")  -- Environment variables
+require(config_dir + ".startup")  -- GUI applications on start
+require(config_dir + ".monitors")  -- Monitor configuration
+require(config_dir + ".settings")  -- Compositor settings
+require(config_dir + ".keybinds")  -- Keybindings for various actions
