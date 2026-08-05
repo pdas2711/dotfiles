4e73df6ccb4682417a879dfa7235f4b775b3133b
Pial Das <pdas2711@proton.me>
2026-08-05 14:25:44 -0400

    Used the correct string concatenation operator for Lua.

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -1,8 +1,8 @@
 -- Config location
 local config_dir = "configs_lua"
 
-require(config_dir + ".env_vars")  -- Environment variables
-require(config_dir + ".startup")  -- GUI applications on start
-require(config_dir + ".monitors")  -- Monitor configuration
-require(config_dir + ".settings")  -- Compositor settings
-require(config_dir + ".keybinds")  -- Keybindings for various actions
+require(config_dir .. ".env_vars")  -- Environment variables
+require(config_dir .. ".startup")  -- GUI applications on start
+require(config_dir .. ".monitors")  -- Monitor configuration
+require(config_dir .. ".settings")  -- Compositor settings
+require(config_dir .. ".keybinds")  -- Keybindings for various actions
