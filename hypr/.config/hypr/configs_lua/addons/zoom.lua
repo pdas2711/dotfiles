32b6398a6c75149bbd2ca747bed58978fc4b3235
Pial Das <pdas2711@proton.me>
2026-08-06 12:36:14 -0400

    Added module for controlling zoom settings.

---
+++ hypr/.config/hypr/configs_lua/addons/zoom.lua
@@ -0,0 +1,22 @@
+---- Zoom Control
+
+
+local module = {}
+
+local current_zoom = hl.get_config("cursor.zoom_factor")
+function module.change_zoom(action)
+	local new_zoom = nil
+	if action == "increment" then
+		new_zoom = current_zoom + 0.2
+	elseif action == "decrement" then
+		new_zoom = current_zoom - 0.2
+		if new_zoom < 1.0 then
+			new_zoom = 1.0
+		end
+	elseif action == "default" then
+		new_zoom = 1.0
+	end
+	hl.config({ cursor = { zoom_factor = new_zoom } })
+end
+
+return module
