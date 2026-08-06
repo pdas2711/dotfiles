030eeb7c17a67c9a97b18a1b17a4fa2d821e6f5c
Pial Das <pdas2711@proton.me>
2026-08-06 12:34:40 -0400

    Added module for miscellaneous settings.

---
+++ hypr/.config/hypr/configs_lua/misc.lua
@@ -0,0 +1,25 @@
+---- Miscellaneous settings
+
+
+-- Default wallpaper
+hl.config({
+	misc = {
+		force_default_wallpaper = 0,  -- Set to 0 or 1 to disable the anime mascot wallpapers
+		disable_hyprland_logo = true  -- If true disables the random hyprland logo / anime girl background
+	}
+})
+
+-- Dwindle Layout
+hl.config({
+	general = {
+		layout = "dwindle"
+	},
+	dwindle = {
+		preserve_split = true,
+		special_scale_factor = 0.92
+	}
+})
+
+-- Setting Applications
+terminal = "kitty"
+lockscreen = "hyprlock"
