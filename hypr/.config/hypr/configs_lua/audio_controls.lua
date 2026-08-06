5ae4790d9e00e76bea5e4a28e1f4d8bd33cee0a6
Pial Das <pdas2711@proton.me>
2026-08-06 12:32:24 -0400

    Added keybinds module for audio_controls to keep bulk of code separate from main keybinds module.

---
+++ hypr/.config/hypr/configs_lua/audio_controls.lua
@@ -0,0 +1,15 @@
+---- Basic audio controls
+
+-- Using the volume knob
+
+hl.bind("XF86AudioRaiseVolume", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })  -- Increase volume by 5%
+hl.bind("XF86AudioLowerVolume", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"), { locked = true, repeating = true })  -- Decrease volume by 5%
+hl.bind("XF86AudioMute", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"), { locked = true, repeating = true })  -- Toggle between mute and unmute audio output
+hl.bind("XF86AudioMicMute",hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"), { locked = true, repeating = true })  -- Toggle between mute and unmute audio input aka mic
+
+-- Using key combinations
+
+hl.bind(main_mod .. " + EQUAL", hl.dsp.exec_cmd("wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+"), { locked = true, repeating = true })  -- Increase volume by 5%
+hl.bind(main_mod .. " + MINUS", hl.dsp.exec_cmd("wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"), { locked = true, repeating = true })  -- Decrease volume by 5%
+hl.bind(main_mod .. " + SHIFT + M", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"), { locked = true, repeating = true })  -- Toggle between mute and unmute audio output
+hl.bind(main_mod .. "+ M", hl.dsp.exec_cmd("wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"), { locked = true, repeating = true })  -- Toggle between mute and unmute audio input aka mic
