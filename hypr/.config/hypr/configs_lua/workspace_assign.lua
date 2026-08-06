92cff2948a14f39716ee78822982562a53f7ec2b
Pial Das <pdas2711@proton.me>
2026-08-06 16:24:18 -0400

    Moved code into a function and called after hyprland fully starts up.

--- hypr/.config/hypr/configs_lua/workspace_assign.lua
+++ hypr/.config/hypr/configs_lua/workspace_assign.lua
@@ -11,32 +11,37 @@ local function monitor_exists(monitor_list, monitor)
 	return nil
 end
 
-local monitors = { "DP-1", "DP-2" }
-local ws_count = 1
+local function set_workspaces()
+	local monitors = { "DP-1", "DP-2" }
+	local ws_count = 1
 
-local found_monitors = hl.get_monitors()
-local ws_per_monitor = 9 // #monitors
-local rem_monitor = 9 % #monitors
-local monitor_default = true
+	local found_monitors = hl.get_monitors()
+	local ws_per_monitor = 9 // #monitors
+	local rem_monitor = 9 % #monitors
+	local monitor_default = true
 
-for _, each_monitor in ipairs(monitors) do
-	if monitor_exists(found_monitors, each_monitor) then
-		for i = 1, ws_per_monitor do
-			if monitor_default then
-				hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = true })
-				monitor_default = false
-			else
-				hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = false})
-			end
-			ws_count = ws_count + 1
-		end
-		if rem_monitor ~= 0 then
-			for i = 1, rem_monitor do
-				hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = false})
+	for _, each_monitor in ipairs(monitors) do
+		if monitor_exists(found_monitors, each_monitor) then
+			for i = 1, ws_per_monitor do
+				hl.print(ws_count)
+				if monitor_default then
+					hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = true })
+					monitor_default = false
+				else
+					hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = false})
+				end
 				ws_count = ws_count + 1
 			end
-			rem_monitor = 0
+			if rem_monitor ~= 0 then
+				for i = 1, rem_monitor do
+					hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = false})
+					ws_count = ws_count + 1
+				end
+				rem_monitor = 0
+			end
 		end
+		monitor_default = true
 	end
-	monitor_default = true
 end
+
+hl.on("hyprland.start", function() set_workspaces() end)
