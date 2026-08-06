70b75eab25be9305efe9a0826b142110c0c06c45
Pial Das <pdas2711@proton.me>
2026-08-06 12:31:14 -0400

    Changed config_dir to global. Added modules and removed redundant settings module.

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -1,8 +1,11 @@
--- Config location
-local config_dir = "configs_lua"
+---- Config location
+
+config_dir = "configs_lua"
 
 require(config_dir .. ".env_vars")  -- Environment variables
+require(config_dir .. ".misc")
 require(config_dir .. ".startup")  -- GUI applications on start
 require(config_dir .. ".monitors")  -- Monitor configuration
-require(config_dir .. ".settings")  -- Compositor settings
 require(config_dir .. ".keybinds")  -- Keybindings for various actions
+require(config_dir .. ".input")
+require(config_dir .. ".decorations")
