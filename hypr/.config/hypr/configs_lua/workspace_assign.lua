a80af75f3778578ba85a6852d79cd2fb13f40f81
Pial Das <pdas2711@proton.me>
2026-08-06 13:54:27 -0400

    Added dynamic workspace assignment. Presently. there's a bug where workspace 3 and 4 are on a monitor not in the list.

---
+++ hypr/.config/hypr/configs_lua/workspace_assign.lua
@@ -0,0 +1,42 @@
+---- Workspace Assignment for defined monitors
+
+
+-- Function to check if a monitor exists. Returns the monior if exists, else, returns nil
+local function monitor_exists(monitor_list, monitor)
+	for _, hl_obj in ipairs(monitor_list) do
+		if hl_obj.name == monitor then
+			return monitor
+		end
+	end
+	return nil
+end
+
+local monitors = { "DP-1", "DP-2" }
+local ws_count = 1
+
+local found_monitors = hl.get_monitors()
+local ws_per_monitor = 9 // #monitors
+local rem_monitor = 9 % #monitors
+local monitor_default = true
+
+for _, each_monitor in ipairs(monitors) do
+	if monitor_exists(found_monitors, each_monitor) then
+		for i = 1, ws_per_monitor do
+			if monitor_default then
+				hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = true })
+				monitor_default = false
+			else
+				hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = false})
+			end
+			ws_count = ws_count + 1
+		end
+		if rem_monitor ~= 0 then
+			for i = 1, rem_monitor do
+				hl.workspace_rule({ workspace = tostring(ws_count), monitor = each_monitor, default = false})
+				ws_count = ws_count + 1
+			end
+			rem_monitor = 0
+		end
+	end
+	monitor_default = true
+end
