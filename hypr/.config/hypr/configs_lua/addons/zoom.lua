a0eccb30ae5e630855f2232c06df17448ff07ffa
Pial Das <pdas2711@proton.me>
2026-08-06 21:18:13 -0400

    Moved retrieving the current zoom into the function since it was not getting called when function was being called.

--- hypr/.config/hypr/configs_lua/addons/zoom.lua
+++ hypr/.config/hypr/configs_lua/addons/zoom.lua
@@ -3,8 +3,8 @@
 
 local module = {}
 
-local current_zoom = hl.get_config("cursor.zoom_factor")
 function module.change_zoom(action)
+	local current_zoom = hl.get_config("cursor.zoom_factor")
 	local new_zoom = nil
 	if action == "increment" then
 		new_zoom = current_zoom + 0.2
