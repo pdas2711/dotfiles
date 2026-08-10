3120061280f602baafc92a15df40475c6b9cd179
Pial Das <pdas2711@proton.me>
2026-08-10 09:55:36 -0400

    Path was fixed. Used variable in the path.

--- hypr/.config/hypr/configs_lua/addons/screenshot.lua
+++ hypr/.config/hypr/configs_lua/addons/screenshot.lua
@@ -3,13 +3,13 @@
 
 local module = {}
 function module.capture(type, destination)
-	file = os.date("%Y_%m_%d-%H_%M_%S")
+	filename = os.date("%Y_%m_%d-%H_%M_%S")
 	if type == "window" then
-		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f ${filename}.png -m window")
+		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. ".png -m window")
 	elseif type == "region" then
-		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f ${filename}.png -m region")
+		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. ".png -m region")
 	elseif type == "monitor" then
-		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f ${filename}.png -m output")
+		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. ".png -m output")
 	end
 	hl.notification.create({
 		text = "Screenshot taken",
