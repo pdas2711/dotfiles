0b5ec9b2fb4350c10822ee742e8d653d17bba19c
Pial Das <pdas2711@proton.me>
2026-08-11 08:05:17 -0400

    Moved file extension as part of the variable. Wrapped hl.exec_cmd() in hl.dispatch(). Notification is more descriptive and longer.

--- hypr/.config/hypr/configs_lua/addons/screenshot.lua
+++ hypr/.config/hypr/configs_lua/addons/screenshot.lua
@@ -3,17 +3,17 @@
 
 local module = {}
 function module.capture(type, destination)
-	filename = os.date("%Y_%m_%d-%H_%M_%S")
+	filename = os.date("%Y_%m_%d-%H_%M_%S") .. ".png"
 	if type == "window" then
-		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. ".png -m window")
+		hl.dispatch(hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. " -m window"))
 	elseif type == "region" then
-		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. ".png -m region")
+		hl.dispatch(hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. " -m region"))
 	elseif type == "monitor" then
-		hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. ".png -m output")
+		hl.dispatch(hl.dsp.exec_cmd("hyprshot -o " .. destination .. "/ -f " .. filename .. " -m output"))
 	end
 	hl.notification.create({
-		text = "Screenshot taken",
-		duration = 3000,
+		text = "Screenshot of " .. type .. " saved as " .. filename,
+		duration = 5000,
 		icon = 5,
 		color = "rgb(ff1ea3)",
 		font_size = 14
