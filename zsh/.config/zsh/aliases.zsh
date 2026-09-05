43fedfeaf6e1f5226eb9c98033487630b4810949
Pial Das <pdas2711@proton.me>
2026-09-05 07:11:32 -0400

    Added alias for wlsunset and added some comments.

--- zsh/.config/zsh/aliases.zsh
+++ zsh/.config/zsh/aliases.zsh
@@ -1,5 +1,6 @@
 # Aliases
 
+# Shell aliases
 alias dateform='date +%Y%m%d_%H%M%S'
 alias sudo='sudo '
 alias ls='ls --color=auto -h --group-directories'
@@ -12,10 +13,13 @@ alias cleantex="rm -f *.aux *.log"
 alias less='less -N --line-num-width=5'
 alias rgpg='source ~/.config/zsh/keys_agent.zsh'
 alias nvimr='nvim -R'
-alias hwin='hyprctl clients'
-alias randpw='pwgen -cys 22 | awk "{print $1}"'
-alias xotp='gopass otp -o otp/xansaware/pdas2711'
 alias wake='ssh xwpi "sudo wake"'
 alias wake-local='ssh xwpi-local "sudo wake"'
 alias calendar='cal'
+
+# Application-specific
 alias hyprlock_restore="hyprctl --instance 0 'keyword misc:allow_session_lock_restore 1'; hyprctl --instance 0 'dispatch exec hyprlock'"
+alias bluelight='wlsunset -t 1400 -T 1500'
+alias hwin='hyprctl clients'
+alias randpw='pwgen -cys 22 | awk "{print $1}"'
+alias xotp='gopass otp -o otp/xansaware/pdas2711'
